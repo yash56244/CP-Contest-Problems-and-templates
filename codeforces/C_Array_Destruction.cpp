85fe43f1ce79d49f6a3a9edd76be6b01f6f994a2
yashvc738 <yash56244@gmail.com>
2021-01-20 14:03:33 +0530

    Added till 20/1/21

---
+++ codeforces/C_Array_Destruction.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mod 1000000007
+#define pi 3.1415926535897932384626433832795
+#define FOR(i, n) for (ll(i) = 0; (i) < (n); (i)++)
+#define FORL(i, a, n) for (ll(i) = (a); (i) <= (n); (i)++)
+#define FORR(i, a, n) for (ll(i) = (a); (i) >= (n); (i)--)
+#define FORSQ(i, a, n) for (ll(i) = (a); (i) * (i) <= (n); ++(i))
+#define FOREACH(a, b) for (auto &(a) : (b))
+#define f first
+#define s second
+#define pb push_back
+#define pon pop_back
+#define mp make_pair
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (ll) v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+typedef pair<char, char> pcc;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef priority_queue<ll> PQMAX;
+typedef priority_queue<ll, vector<ll>,
+					   greater<ll>>
+	PQMIN;
+typedef set<ll> setll;
+typedef map<ll, ll> mapll;
+
+ll power(ll x, ll y, ll p)
+{
+	ll res = 1;
+	x = x % p;
+	if (x == 0)
+		return 0;
+	while (y > 0)
+	{
+		if (y & 1)
+			res = (res * x) % p;
+		y = y >> 1;
+		x = (x * x) % p;
+	}
+	return res;
+}
+
+void yash56244()
+{
+	ll n;
+	cin >> n;
+	n *= 2;
+	ll arr[n];
+	ll maxi = 0;
+	FOR(i, n)
+	{
+		cin >> arr[i];
+		if (arr[i] > arr[maxi])
+		{
+			maxi = i;
+		}
+	}
+	bool no = true;
+	FOR(i, n)
+	{
+		if (i == maxi)
+		{
+			continue;
+		}
+		multiset<ll> ms;
+		vll v;
+		FOR(j, n)
+		{
+			ms.insert(arr[j]);
+		}
+		ll x = arr[i] + arr[maxi];
+		bool flag = true;
+		while (!ms.empty())
+		{
+			ll mx = *ms.rbegin();
+			ms.erase(ms.find(mx));
+			if (ms.find(x - mx) == ms.end())
+			{
+				flag = false;
+				break;
+			}
+			ms.erase(ms.find(x - mx));
+			v.push_back(make_pair(mx, x - mx));
+			x = max(mx, x - mx);
+		}
+		if (flag)
+		{
+			cout << "YES" << endl;
+			cout << arr[maxi] + arr[i] << endl;
+			FOREACH(a, v)
+			{
+				cout << a.first << " " << a.second << endl;
+			}
+			no = false;
+			break;
+		}
+	}
+	if (no)
+	{
+		cout << "NO" << endl;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll t = 1;
+	cin >> t;
+	while (t--)
+	{
+		yash56244();
+	}
+	return 0;
+}
