71007d1aa75b4534431d50e9050f60d66352280f
yashvc738 <yash56244@gmail.com>
2021-02-18 13:27:09 +0530

    Added till 18/2/21

---
+++ codeforces/C_Sum_of_Cubes.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mod 1e9 + 7
+#define pi 3.1415926535897932384626433832795
+#define endl "\n"
+#define FOR(i, n) for (ll(i) = 0; (i) < (n); (i)++)
+#define FORL(i, a, n) for (ll(i) = (a); (i) <= (n); (i)++)
+#define FORR(i, a, n) for (ll(i) = (a); (i) >= (n); (i)--)
+#define FORSQ(i, a, n) for (ll(i) = (a); (i) * (i) <= (n); ++(i))
+#define FOREACH(a, b) for (auto &(a) : (b))
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (ll) v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+
+void err(istream_iterator<string> it)
+{
+}
+
+template <typename T, typename... Args>
+
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+typedef pair<char, char> pcc;
+typedef vector<bool> vb;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef priority_queue<ll> PQMAX;
+typedef priority_queue<ll, vector<ll>,
+                       greater<ll>>
+    PQMIN;
+typedef set<ll> setll;
+typedef map<ll, ll> mapll;
+
+const ll inf = 1e15;
+
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;
+    x = x % p;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+void yash56244()
+{
+    ll n;
+    cin >> n;
+    unordered_map<ll, ll> s;
+    bool flag = false;
+    for (ll i = 1; i * i * i <= n; i++)
+    {
+        s[i * i * i] = 1;
+        if (s.find(n - (i * i * i)) != s.end())
+        {
+            cout << "YES" << endl;
+            flag = true;
+        }
+    }
+    if (!flag)
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        yash56244();
+    }
+    return 0;
+}
